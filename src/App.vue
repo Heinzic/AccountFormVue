f2af04dc8bde9417393383cbf33de5d6e9548680
Daniil Ryzhenko <ryzhenko62@gmail.com>
2025-10-07 16:25:39 +0300

    app fix

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,8 @@
   </div>
 </template>
 
-<script lang="ts">
-  import { defineComponent } from 'vue';
+<script setup lang="ts">
   import AccountsList from './components/AccountsList.vue';
-
-  export default defineComponent({
-    name: 'App',
-    components: { AccountsList }
-  });
 </script>
 
 <style scoped>
